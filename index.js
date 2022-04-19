65fcbcafa4515019f660651b08f7a59da15029f8
Braden Hansen <71408639+bradenkh@users.noreply.github.com>
2022-04-19 12:25:17 -0600

    updated index to support paths

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
-var http = require("http"),
-   fileSystem = require("fs"),
-   path = require("path");
+// var http = require("http"),
+//    fileSystem = require("fs"),
+//    path = require("path");
 
-http
-   .createServer(function (request, response) {
-      var filePath = "./oneFormFix.js";
+// http
+//    .createServer(function (request, response) {
+//       var filePath = "./oneFormFix.js";
 
-      response.writeHead(200, {
-         "Content-Type": "text/javascript",
-      });
+//       response.writeHead(200, {
+//          "Content-Type": "text/javascript",
+//       });
 
-      var readStream = fileSystem.createReadStream(filePath);
-      // We replaced all the event handlers with a simple call to readStream.pipe()
-      readStream.pipe(response);
-   })
-   .listen(process.env.PORT || 8080);
+//       var readStream = fileSystem.createReadStream(filePath);
+//       // We replaced all the event handlers with a simple call to readStream.pipe()
+//       readStream.pipe(response);
+//    })
+//    .listen(process.env.PORT || 8080);
+
+const path = require("path");
+const express = require("express");
+const fs = require("fs");
+const Server = require("http");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.get("/", (req, res) => {
+   res.sendFile(path.join(__dirname, "/oneFormFix.js"));
+});
+
+app.listen(PORT);
