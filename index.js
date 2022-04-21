1d082c89ff51a5ecb79e7fc2290f7bf427f34c4f
Braden Hansen <71408639+bradenkh@users.noreply.github.com>
2022-04-21 10:31:23 -0600

    fixed parenthesis bug

--- index.js
+++ index.js
@@ -11,13 +11,9 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-app.get(
-   "/update",
-   (req,
-   (res) => {
-      res.send({ message: "call was successful" });
-   })
-);
+app.get("/update", (req, res) => {
+   res.send({ message: "call was successful" });
+});
 
 app.get("/phones", (req, res) => {
    res.sendFile(path.join(__dirname, "/oneFormFixPhones.js"));
